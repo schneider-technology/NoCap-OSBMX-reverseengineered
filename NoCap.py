02aa3b334f1e5bcbaad6b5e003c49faed2e9dcd4
schneider-technology <schneiderkevin@gmx.net>
2025-02-22 00:53:20 +0100

    Done, only fillet to the stem is missing, but AFAIK it doesn't serve any purpose anyway

--- NoCap.py
+++ NoCap.py
@@ -22,6 +22,8 @@ slop_h_stem = 0.2  # Increase if stem doesn't go all the way into the stem shaft
 
 h_stem_slot_chamfer = 0.75 # Height of stem slot chamfer.
 l_stem_slot_chamfer = 0.35 # Length of stem slot chamfer.
+stem_base_height = 0.8  # Height of stem base.
+stem_radius = 1.0  # Radius of stem base fillet.
 
 '''Don't change anything after here unless you need to.'''
 
@@ -48,16 +50,28 @@ with BuildPart() as cap:
         Circle(d_cap / 2 - t_cap_walls)
     extrude(amount=-(h_cap - t_cap_top), mode=Mode.SUBTRACT)
 
+    # Debugging: Print out all edges and their properties
+    print("Edges after hollowing out the keycap:")
+    for i, edge in enumerate(cap.edges()):
+        print(f"Edge {i}: {edge}")
+        print(f"  Length: {edge.length}")
+        print(f"  Position: {edge.position}")
+
+    # Fillet the inside circle of the keycap by 2mm.
+    inside_circle_edge = cap.edges().filter_by_position(Axis.Z, t_cap_top, t_cap_top)[0]  # Filter by Z position
+    print(f"Selected edge for filleting: {inside_circle_edge}")
+    fillet(inside_circle_edge, radius=2)
+
     # Create sketch of the stem on the inside of the keycap's top.
     cap_top_inside = cap.faces().filter_by(Axis.Z).sort_by(Axis.Z)[-2]
     with BuildSketch(cap_top_inside) as stem_sk:
         stem_shaft = Rectangle(width=l_shaft, height=w_shaft)
-        fillet(stem_shaft.vertices(), radius=1.0)
+        fillet(stem_shaft.vertices(), radius=stem_radius)
         Rectangle(width=t_stem, height=w_shaft, mode=Mode.SUBTRACT)
         Rectangle(width=l_stem, height=t_stem, mode=Mode.SUBTRACT)
     stem = extrude(amount=h_shaft)
 
-    # Select and chamfer the top inner edges of the stem, excluding one side.
+    # Select and chamfer the top inner edges of the stem slot.
     stem_top_inner_edges = (
         stem.edges()
         .group_by(Axis.Z)[-1]
@@ -65,6 +79,12 @@ with BuildPart() as cap:
         .filter_by_position(Axis.Y, -w_shaft / 2.0, w_shaft / 2.05, (False, False))
     )
     chamfer(stem_top_inner_edges, h_stem_slot_chamfer, l_stem_slot_chamfer)
+
+    # Build a base for the stem to sit on.
+    with BuildSketch(cap_top_inside) as stem_base_sk:
+        stem_base_shaft = Rectangle(width=l_shaft, height=w_shaft)
+        fillet(stem_base_shaft.vertices(), radius=stem_radius)
+    extrude(amount=stem_base_height)
 
     # Cut notches in switch corner positions to prevent the walls colliding
     # with the switch.
